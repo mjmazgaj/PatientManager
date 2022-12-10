58cc72ebea2b86e4e4bfa9e063598f75a3c3e6e1
Mazgaj <>
2022-12-10 23:57:06 +0100

    Added MedicineModel converter

---
+++ PatientManager.Data/Services/Convertion/MedicineModelConvert.cs
@@ -0,0 +1,40 @@
+﻿using PatientManager.Core;
+using System.Text.Json.Nodes;
+
+namespace PatientManager.Data
+{
+    public static class MedicineModelConvert
+    {
+        //TODO
+        //MedicineJsonModel has only Patient's Ids, MedicineModel should have list of PatientModels.
+        public static MedicineModel ToMedicineModel(MedicineJsonModel jsonModel)
+        {
+            var medicineModel = new MedicineModel();
+
+            if (medicineModel != null)
+            {
+                medicineModel.Id = jsonModel.Id;
+                medicineModel.Name = jsonModel.Name;
+                medicineModel.Patients = jsonModel.PatientIds;
+                medicineModel.Description = jsonModel.Description;
+            };
+
+            return medicineModel;
+        }
+
+        public static MedicineJsonModel ToMedicineJsonModel(MedicineModel medicineModel)
+        {
+            var medicineJsonModel = new MedicineJsonModel();
+
+            if (medicineJsonModel != null)
+            {
+                medicineJsonModel.Id = medicineModel.Id;
+                medicineJsonModel.Name = medicineModel.Name;
+                medicineJsonModel.PatientIds = medicineModel.Patients;
+                medicineJsonModel.Description = medicineModel.Description;
+            };
+
+            return medicineJsonModel;
+        }
+    }
+}
