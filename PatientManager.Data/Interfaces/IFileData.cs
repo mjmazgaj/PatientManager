e7d45155d4907a82b44dd00fe2f907d3fa4f7b1e
Mazgaj <>
2022-12-10 23:58:27 +0100

    Added Delete and Edit JsonMedicine functionality

--- PatientManager.Data/Interfaces/IFileData.cs
+++ PatientManager.Data/Interfaces/IFileData.cs
@@ -7,5 +7,7 @@ namespace PatientManager.Data
     {
         public void AddJsonObject<T>(T newObject, FileNameType fileNameType);
         public List<T> GetJsonObjects<T>(FileNameType fileNameType);
+        public void EditJsonMedicine(MedicineJsonModel newObject);
+        public void DeleteJsonMedicine(int id);
     }
 }
--- PatientManager.Data/Services/FileDataService.cs
+++ PatientManager.Data/Services/FileDataService.cs
@@ -20,6 +20,37 @@ namespace PatientManager.Data
             File.WriteAllText($"{fileNameType}.json", $"{jsonString}");
         }
 
+        public void DeleteJsonMedicine(int id)
+        {
+            FileNameType fileNameType = FileNameType.Medicine;
+            file = $"{fileNameType}.json";
+            List<MedicineJsonModel> collection = GetJsonObjects<MedicineJsonModel>(fileNameType).ToList();
+
+            collection = collection.Where(x => x.Id != id).ToList();
+
+            string jsonString = JsonSerializer.Serialize(collection, options);
+            File.WriteAllText($"{fileNameType}.json", $"{jsonString}");
+        }
+
+        public void EditJsonMedicine(MedicineJsonModel newObject)
+        {
+            FileNameType fileNameType = FileNameType.Medicine;
+            file = $"{fileNameType}.json";
+            List<MedicineJsonModel> collection = GetJsonObjects<MedicineJsonModel>(fileNameType).ToList();
+
+            MedicineJsonModel model = collection.FirstOrDefault(x => x.Id == newObject.Id);
+
+            if (model != null)
+            {
+                model.PatientIds = newObject.PatientIds;
+                model.Name = newObject.Name;
+                model.Description = newObject.Description;
+            }
+
+            string jsonString = JsonSerializer.Serialize(collection, options);
+            File.WriteAllText($"{fileNameType}.json", $"{jsonString}");
+        }
+
         public List<T> GetJsonObjects<T>(FileNameType fileNameType)
         {
             file = $"{fileNameType}.json";
