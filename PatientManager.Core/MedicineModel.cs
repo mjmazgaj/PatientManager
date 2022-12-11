20cd52dc38c9c6ee86e4b42863a6850f492eae09
Mazgaj <>
2022-12-11 14:18:52 +0100

    Remove redundant line in MedicineModel

--- PatientManager.Core/MedicineModel.cs
+++ PatientManager.Core/MedicineModel.cs
@@ -5,7 +5,6 @@
         //TODO
         //JSON file contains only patient's Ids. Once the PatientModel is created List<PatientModel> is implemented.
         public List<int> Patients { get; set; }
-        //public int NumberOfPatients => Patients.Count;
         public int? NumberOfPatients { get => Patients?.Count; }
         public string Description { get; set; }
     }
