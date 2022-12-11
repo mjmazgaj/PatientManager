2645f42091a1d16a2dd0a7ff4f7ded202bf2f073
Mazgaj <>
2022-12-11 17:21:21 +0100

    Refactor model converter

--- PatientManager.Data/Services/Convertion/PatientModelConvert.cs
+++ PatientManager.Data/Services/Convertion/PatientModelConvert.cs
@@ -8,10 +8,12 @@ namespace PatientManager.Data
         //PatientJsonModel has only Patient's Ids, PatientModel should have list of TreatmentModels.
         public static PatientModel ToPatientModel(PatientJsonModel jsonModel)
         {
-            var patientModel = new PatientModel();
+            PatientModel patientModel = null;
 
-            if (patientModel != null && jsonModel != null)
+            if (jsonModel != null)
             {
+                patientModel = new PatientModel();
+
                 patientModel.Id = jsonModel.Id;
                 patientModel.Name = jsonModel.Name;
                 patientModel.Treatments = jsonModel.TreatmentIds;
@@ -23,10 +25,12 @@ namespace PatientManager.Data
 
         public static PatientJsonModel ToPatientJsonModel(PatientModel patientModel)
         {
-            var patientJsonModel = new PatientJsonModel();
+            PatientJsonModel patientJsonModel = null;
 
-            if (patientJsonModel != null && patientModel != null)
+            if (patientModel != null)
             {
+                patientJsonModel = new PatientJsonModel();
+
                 patientJsonModel.Id = patientModel.Id;
                 patientJsonModel.Name = patientModel.Name;
                 patientJsonModel.TreatmentIds = patientModel.Treatments;
