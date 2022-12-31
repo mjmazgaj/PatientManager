e814236fd84b391737223fa1b9bc890120339085
Mazgaj <>
2022-12-31 17:50:36 +0100

    Added Count and Unit properties to MedicineModel

--- PatientManager.Core/MedicineModel.cs
+++ PatientManager.Core/MedicineModel.cs
@@ -1,7 +1,15 @@
-﻿namespace PatientManager.Core
+﻿using PatientManager.Core.Enum;
+
+namespace PatientManager.Core
 {
     public class MedicineModel : BaseModel
     {
+        public string FullName
+        {
+            get { return $"{Name} ({Count} {Unit})"; }
+        }
+        public decimal Count { get; set; }
+        public MedicineUnits Unit { get; set; }
         public string Description { get; set; }
     }
 }
